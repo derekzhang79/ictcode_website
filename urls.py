b0e1a1d85c9c9fb52da9627a991c8ae239f77b8e
William Fagan <will@wichitacode.com>
2011-06-06 10:55:53 -0500

    Add in initial template files for homepage, 404 and 500.

--- urls.py
+++ urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import direct_to_template
 from django.contrib import admin
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', direct_to_template, {'template': 'home.html'}, name='home'),
+    
+    url(r'^404/$', direct_to_template, {'template': '404.html'}, name='404'),
+    url(r'^500/$', direct_to_template, {'template': '500.html'}, name='500'),
+    
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
