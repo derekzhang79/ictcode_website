e8ce458209b5acf0ca4cb5e11374f65067b88dae
William Fagan <will@wichitacode.com>
2011-06-07 23:07:47 -0500

    Add django_utils to the requirements and use it's reset.css.

--- collectstatic.sh
+++ collectstatic.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-yui-compressor --type=css css/style.css > css/style-min.css
-
-cat css/style-min.css >> css/main-min.css
-
 python manage.py collectstatic --noinput
+
+mkdir -p static/min/
+
+yui-compressor --type=css static/django_utils/css/reset.css > static/min/reset-min.css
+yui-compressor --type=css static/style.css > static/min/style-min.css
+
+cat static/min/reset-min.css > static/style.css
+cat static/min/style-min.css >> css/style.css
+
+rm -r static/min/
--- settings.py
+++ settings.py
@@ -67,6 +67,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.admindocs',
+    'django_utils',
 )
 
 LOGGING = {
