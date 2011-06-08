a9b757043949541c4d588ef1cc468d8b05943ad1
William Fagan <will@wichitacode.com>
2011-06-08 17:53:09 -0500

    Update to a different way of handling our stylesheet.

--- collectstatic.sh
+++ collectstatic.sh
@@ -2,12 +2,6 @@
 
 python manage.py collectstatic --noinput
 
-mkdir -p static/min/
+yui-compressor --type=css static/django_utils/css/reset.css css/base.css > css/style.css
 
-yui-compressor --type=css static/django_utils/css/reset.css > static/min/reset-min.css
-yui-compressor --type=css static/style.css > static/min/style-min.css
-
-cat static/min/reset-min.css > static/style.css
-cat static/min/style-min.css >> css/style.css
-
-rm -r static/min/
+python manage.py collectstatic --noinput
